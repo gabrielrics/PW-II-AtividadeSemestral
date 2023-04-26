53698a94f2c28b626a9b89b965e8a54075586e78
gabrielrics <gabriel908henrique@gmail.com>
2023-04-25 21:52:31 -0300

    Iniciando as idéias

---
+++ Tela inicial/rodape.php
@@ -0,0 +1,8 @@
+<hr>   
+    <footer>
+        <h3> Atividade Semestral feita em 2023, matéria: Programação WEB II </h3>
+        <img src= 
+    </footer>
+    
+</body>
+</html>
---
+++ ideias/ideias.java
@@ -0,0 +1,15 @@
+/*
+Filmes 
+Parte 1: Em cartazes;
+Parte 1: Em cartazes;
+Parte 2: Mais famosos;
+Parte 3: Algumas categorias.
+
+Séries:
+
+Parte 1: Em cartazes;
+Parte 2: Mais famosos;
+Parte 3: Algumas categorias.
+
+História:
+História do cinema (Apenas uma Parte)*/
