e67afa7475392bc3fb0b8a70789d343db6aafc49
gabrielrics <gabriel908henrique@gmail.com>
2023-06-14 16:59:31 -0300

    Mudei o CSS

--- Visualizacao/Terror.php
+++ Visualizacao/Terror.php
@@ -1,4 +1,13 @@
 <?php include('cabecalho.php'); ?>
 
+<a href="Terror.php">
+          <div class="flip-card-terror">
+            <div class="flip-card-inner-terror">
+
+              <h2> TERROR </h2>
+            
+            </div>
+          </div>
+        </a>
 
 <?php include('rodape.php'); ?>    
