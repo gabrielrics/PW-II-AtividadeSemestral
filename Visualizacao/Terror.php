fd9d81075dcbb1583a9ea01f0270af789acf2b4e
gabrielrics <gabriel908henrique@gmail.com>
2023-06-14 22:02:15 -0300

    Página de terror

--- Visualizacao/Terror.php
+++ Visualizacao/Terror.php
@@ -1,11 +1,17 @@
 <?php include('cabecalho.php'); ?>
 
-<a href="Terror.php">
-          <div class="flip-card-terror">
-            <div class="flip-card-inner-terror">
+<h1 class='nada'> Classificação para maiores de 12 </h1>
 
-              <h2> TERROR </h2>
+
+        <a href="Terror.php">
+            <div class="flip-card-terror">
+              <div class="poltergeist">
+              <img src="../Images/Poltergeist.jpg" alt="Imagem 1">
+              <div class="flip-card-inner-terror">
+
+              <h2> POLTERGEIST </h2>
             
+              </div>
             </div>
           </div>
         </a>
